88abc4554545dc922feabd3c181e1ca546dac47a
Rich Rowley <fearless359@comcast.net>
2016-10-04 08:46:10 -0700

    Revert "Consolidate invoice functions in Invoice class."
    This reverts commit f5b32534f3307e8fd270b642665fd6f89c295cc2.

--- modules/invoices/save.php
+++ modules/invoices/save.php
@@ -37,13 +37,7 @@ if ($_POST['action'] == "insert" ) {
         Product::insertProduct(0,0);
         $product_id = lastInsertId();
         $tax_id = (empty($_POST["tax_id"][0] ) ? "" : $_POST["tax_id"][0]);
-        Invoice::insertInvoiceItem($id,
-                                   1,
-                                   $product_id,
-                                   1,
-                                   $tax_id,
-                                   $_POST['description'],
-                                   $_POST['unit_price']);
+        insertInvoiceItem($id, 1, $product_id, 1, $tax_id, $_POST['description'], $_POST['unit_price']);
     } elseif ($saved) {
         $i = 0;
         while ($i <= $_POST['max_items']) {
@@ -51,14 +45,9 @@ if ($_POST['action'] == "insert" ) {
                 // @formatter:off
                 $tax_id = (empty($_POST["tax_id"][$i] ) ? "" : $_POST["tax_id"][$i]);
                 $attr = (empty($_POST["attribute"][$i]) ? "" : $_POST["attribute"][$i]);
-                Invoice::insertInvoiceItem($id,
-                                           $_POST["quantity$i"],
-                                           $_POST["products$i"],
-                                           $i,
-                                           $tax_id,
-                                           $_POST["description$i"],
-                                           $_POST["unit_price$i"],
-                                           $attr);
+                insertInvoiceItem($id, $_POST["quantity$i"]  , $_POST["products$i"]   ,
+                                  $i , $tax_id               , $_POST["description$i"],
+                                       $_POST["unit_price$i"], $attr);
                 // @formatter:on
             }
             $i++;
@@ -94,23 +83,9 @@ if ($_POST['action'] == "insert" ) {
                 $attr    = (isset($_POST["attribute$i"]  ) ? $_POST["attribute$i"  ] : "");
                 $tax_id  = (isset($_POST["tax_id$i"]     ) ? $_POST["tax_id$i"     ] : "");
                 if ($item == "") {
-                    Invoice::insertInvoiceItem($id,
-                                               $qty,
-                                               $product,
-                                               $i,
-                                               $tax_id,
-                                               $desc,
-                                               $price,
-                                               $attr);
+                    insertInvoiceItem($id  , $qty, $product, $i, $tax_id, $desc, $price, $attr);
                 } else {
-                    Invoice::updateInvoiceItem($item,
-                                               $qty,
-                                               $product,
-                                               $i,
-                                               $tax_id,
-                                               $desc,
-                                               $price,
-                                               $attr);
+                    updateInvoiceItem($item, $qty, $product, $i, $tax_id, $desc, $price, $attr);
                 }
                 // @formatter:on
             }
